b0e97ae60c62fd181a6779ed8064c9563c9666d6
José Dall'Asta <69054878+nekitbr@users.noreply.github.com>
2026-01-19 12:45:25 -0300

    Add files via upload

---
+++ src/main/java/com/example/demo/MyController.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class MyController {
+
+    @GetMapping
+    public String getString() {
+        return "Hello World!";
+    }
+
+}
